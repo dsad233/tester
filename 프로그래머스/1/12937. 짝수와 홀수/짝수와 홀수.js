682adf3570f36ffed4abb7666a1a53cfc76dd30a
portyourself <94047981+dsad233@users.noreply.github.com>
2024-01-10 09:17:00 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,11 +1,9 @@
 function solution(num) {
-  var answer = '';
-  if( num % 2 === 0){
-    answer = "Even";
-  }else{
-    answer = "Odd";
-  }
-  return answer;
+    var answer = '';
+    if(num % 2 === 0){
+        answer = "Even";
+    } else{
+        answer = "Odd";
+    }
+    return answer;
 }
-const result = solution(4);
-console.log(result);
