aed9c0f2e5f9854d453fdba61ac143c1b3ba15dc
portyourself <94047981+dsad233@users.noreply.github.com>
2024-01-25 09:16:59 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,6 +1,5 @@
 function solution(seoul) {
     var answer = seoul.indexOf('Kim');
-    let tell = "김서방은 "+ answer+"에 있다";
+    let tell = `김서방은 ${answer}에 있다`;
     return tell;
 }
-solution(["Jane", "Kim"]);
