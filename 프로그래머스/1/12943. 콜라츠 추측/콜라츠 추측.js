4080f981e0d1431a9b8d721b7cc0608ddea4c4b8
portyourself <94047981+dsad233@users.noreply.github.com>
2024-01-25 09:08:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,9 @@
 function solution(num) {
     var answer = 0;
-    while( num !== 1 ){
+    while ( num !== 1){
         if( num % 2 === 0){
             num /= 2;
-        } else{
+        } else {
             num = num * 3 + 1;
         }
         answer ++;
