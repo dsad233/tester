2e2a70780fa7ee8861e36cd9bb398d9d2c2b86cf
portyourself <94047981+dsad233@users.noreply.github.com>
2024-05-14 20:20:46 +0900

    Update 문자열안에 문자열.js

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,12 +1,9 @@
 function solution(str1, str2) {
     var answer = 0;
-    for(let i = 0; i < str1.length; i++){
-        if(str1.includes(str2)){
-            return answer = 1;
+    if(str1.includes(str2)){
+        return answer = 1;
     } else {
         return answer = 2;
     }
-    }
-    
     return answer;
 }
