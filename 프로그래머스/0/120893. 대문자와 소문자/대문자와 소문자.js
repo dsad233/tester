9542fe287eb9cf08e74aaf78b7538d35172af680
portyourself <94047981+dsad233@users.noreply.github.com>
2024-05-15 19:38:44 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++){
+        if(my_string[i].toLowerCase() === my_string[i]){
+            answer += my_string[i].toUpperCase();
+        } else if(my_string[i].toUpperCase() === my_string[i]){
+            answer += my_string[i].toLowerCase();
+        }
+    }
+    return answer;
+}
