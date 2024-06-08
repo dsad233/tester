12fe342483f853ca3d9a3b447b37199cb1c466a7
portyourself <94047981+dsad233@users.noreply.github.com>
2024-06-09 01:04:09 +0900

    Update 가까운 1 찾기.js

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -2,7 +2,7 @@ function solution(arr, idx) {
     var answer = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === 1 && i >= idx){
-            answer = Math.min(i);
+            answer = i;
             break;
         } else {
             answer = -1;
