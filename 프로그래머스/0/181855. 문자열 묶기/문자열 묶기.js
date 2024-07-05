52673621cb0ae01bbf1ed75bf61339e607a11b73
portyourself <94047981+dsad233@users.noreply.github.com>
2024-07-05 12:54:50 +0900

    [level 0] Title: 문자열 묶기, Time: 2.76 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,16 +1,29 @@
 function solution(strArr) {
     var answer = 0;
     let count = {};
-    for(let key of strArr){
-        let length = key.length;
+    
+    for(let i = 0; i < strArr.length; i++){
+        let length = strArr[i].length;
         if(!count[length]){
             count[length] = 0;
         }
         count[length]++;
     }
     
-    let max = Math.max(...Object.values(count));
-    answer = max;
+    let keys = Object.keys(count);
+    let oneKeys = keys[0];
     
+    for(let j = 0; j < keys.length; j++){  
+        if(count[j] > count[oneKeys]){
+            oneKeys = count[j];
+        }
+    }
+    
+    let maxValues = Math.max(...Object.values(count));
+    
+    if(oneKeys && maxValues){
+        answer = maxValues;
+    }
+
     return answer;
 }
