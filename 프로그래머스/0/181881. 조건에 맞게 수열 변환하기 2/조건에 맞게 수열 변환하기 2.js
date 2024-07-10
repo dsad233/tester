0685373c3e8e9b55a88c7988f2dd40098922f210
portyourself <94047981+dsad233@users.noreply.github.com>
2024-07-10 15:46:42 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,20 +1,23 @@
 function solution(arr) {
     var answer = 0;
+    
     while(true){
         let array = [...arr];
+        let count = [];
         for(let i = 0; i < arr.length; i++){
             if(arr[i] >= 50 && arr[i] % 2 === 0){
                 arr[i] /= 2;
             } else if(arr[i] < 50 && arr[i] % 2 === 1){
-                arr[i] = (arr[i] * 2) + 1;
+                arr[i] = (arr[i] * 2) + 1; 
             }
         }
-        
-        if(array.toString() === arr.toString()){
+        count.push(arr);
+    
+        if(array.toString() === count.toString()){
             break;
         }
-            
         answer++;
     }
+    
     return answer;
 }
