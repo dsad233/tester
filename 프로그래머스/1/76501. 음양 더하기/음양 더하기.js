b256d1165c40ae29a990f1a13337fb3b94505585
portyourself <94047981+dsad233@users.noreply.github.com>
2024-09-13 21:54:28 +0900

    [level 1] Title: 음양 더하기, Time: 6.53 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,16 @@
 function solution(absolutes, signs) {
     var answer = 0;
+    
+    console.log("absolutes : ",absolutes);
+    console.log("signs : ", signs);
+    
     for(let i = 0; i < absolutes.length; i++){
-    if( signs[i] ){
-        answer += absolutes[i];
-    } else{
-        answer -= absolutes[i];
+        if(signs[i]){
+            answer += absolutes[i];
+        } else {
+            answer -= absolutes[i];
+        }
     }
-    }
+    
     return answer;
 }
