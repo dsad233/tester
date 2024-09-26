330d21c56b2c437e82daade09b0dc4a491cba871
portyourself <94047981+dsad233@users.noreply.github.com>
2024-09-26 16:31:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.27 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,13 +1,16 @@
 function solution(arr) {
     var answer = [];
+    const minNum = Math.min(...arr);
     
-    if(arr[0] === 10 && arr.length === 1 ){
+    if(arr.length === 0 || arr[0] === 10){
         answer.push(-1);
     }
+    
     for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== Math.min(...arr)){
+        if(arr[i] !== minNum){
             answer.push(arr[i]);
         }
     }
+    
     return answer;
 }
