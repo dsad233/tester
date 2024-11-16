b757abf3a1b70cd5a065e2c4b68cd05f8b671ead
portyourself <94047981+dsad233@users.noreply.github.com>
2024-11-16 20:07:24 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    var answer = 0;
+    let result = 0;
+    for(let i = 0; i < numbers.length; i++){
+        result += numbers[i];
+    }
+    
+    answer = result / numbers.length;
+    return answer;
+}
