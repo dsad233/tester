afd750e47e7acf1238070d1ff29b4f1694b0d007
portyourself <94047981+dsad233@users.noreply.github.com>
2024-12-17 14:03:55 +0900

    Update 취소되지 않은 진료 예약 조회하기.sql
    수정

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,4 +1,4 @@
-SELECT a.APNT_NO as "APNTNO", p.PT_NAME as "PTNAME", p.PT_NO as "PTNO", d.MCDP_CD as "MCDPCD", d.DR_NAME as "DRNAME", a.APNT_YMD as "APNTYMD"
+SELECT a.APNT_NO as "APNTNO", p.PT_NAME as "PTNAME", p.PT_NO as "PTNO", a.MCDP_CD as "MCDPCD", d.DR_NAME as "DRNAME", a.APNT_YMD as "APNTYMD"
 from PATIENT p inner join APPOINTMENT a on p.PT_NO = a.PT_NO inner join DOCTOR d on d.DR_ID = a.MDDR_ID
 where DATE_FORMAT(a.APNT_YMD, "%Y-%m-%d") = "2022-04-13" and a.APNT_CNCL_YN = "N"
 order by 6 asc
